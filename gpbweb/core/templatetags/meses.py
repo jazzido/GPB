a72d86e214c2c94dbbd762a97697f52c0df486da
Manuel Aristarán <jazzido@jazzido.com>
2010-07-01 16:27:41 -0300

    meses tag

--- gpbweb/core/templatetags/meses.py
+++ gpbweb/core/templatetags/meses.py
@@ -1,29 +1,44 @@
 from django import template
-from datetime import datetime
+from datetime import datetime, date
 import re
 
 register = template.Library()
 
 class MesesNode(template.Node):
-    def __init__(self, varname, current=datetime.now(), start_year=datetime.now().year):
-        self.current_time = current
-        self.varname = varname
+    def __init__(self, start_varname, end_varname, template_varname):
+        self.start_var = template.Variable(start_varname)
+        self.end_var = template.Variable(end_varname)
+        self.template_varname = template_varname
 
     def render(self, context):
-        context[self.varname] = []
-        for y in reversed(range(start_year, self.current_time.year+1)):
-            context[self.varname] = [datetime(y, m, 1) for m in reversed(range(1, self.current_time.month+1 if self.current_time.year == y else 12+1))]
+        meses = []
+        start = self.start_var.resolve(context).date()
+        end = self.end_var.resolve(context).date()
+
+        m = start.month
+        y = start.year
+        d = date(y, m, 1)
+        while True:
+            if d > end: break
+            meses.append(d)
+            m += 1
+            if m % 12 == 1:
+                y += 1
+            d = date(y, m % 12 if m % 12 != 0 else 12, 1)
+        
+        context[self.template_varname] = meses
         return ''
 
 def do_meses(parser, token):
     try:
         # Splitting by None == splitting by spaces.
-        tag_name, arg = token.contents.split(None, 1)
+        tag_name, _from, start_varname, _until, end_varname, _as, template_varname = token.contents.split(None, 6)
     except ValueError:
         raise template.TemplateSyntaxError, "%r tag requires arguments" % token.contents.split()[0]
 
-    m = re.search(r'from (?P<start>.+) until (?P<until_year>.+) as (?P<variable_name>\w+)', arg)
+    return MesesNode(start_varname, end_varname, template_varname)
     
+register.tag('month_range', do_meses)
 
     
 
